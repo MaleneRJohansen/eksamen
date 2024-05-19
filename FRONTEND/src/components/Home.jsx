cf02a1413be8cbc1f62c662d00cb1c3a58380bf3
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-05-19 09:24:07 +0200

    lagt til api til Home og grids i App.css

--- FRONTEND/src/components/Home.jsx
+++ FRONTEND/src/components/Home.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Type from './Type'
 import Pokemon from './Pokemon'
 
 function Home() {
+  const [pokemons, setPokemons] = useState([]);
+  const [types, setTypes] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const responsePokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=9');
+        const dataPokemons = await responsePokemons.json();
+        
+        const pokemonDetails = await Promise.all(
+          dataPokemons.results.map(async (pokemon) => {
+            const response = await fetch(pokemon.url);
+            return response.json();
+          })
+        );
+        setPokemons(pokemonDetails);
+
+        const responseTypes = await fetch('https://pokeapi.co/api/v2/type?limit=18');
+        const dataTypes = await responseTypes.json();
+        setTypes(dataTypes.results);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
   return (
     <>
       <h1>Home</h1>
       <section>
         <h2>Main Pokemons</h2>
-        <p>Her skal de 9 først pokemons fra API-et vises</p>
+        <div className="grid-container main-pokemons-grid">
+          {pokemons.map((pokemon) => (
+            <div key={pokemon.name} className="grid-item">
+              <p>{pokemon.name}</p>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
+          ))}
+        </div>
       </section>
       <section>
         <h2>Types</h2>
-        <p>Her skal alle typer vises</p>
+        <div className="grid-container types-grid">
+          {types.map((type) => (
+            <div key={type.name} className="grid-item">
+              <p>{type.name}</p>
+            </div>
+          ))}
+        </div>
       </section>
     </>
-  )
+  );
 }
-
-export default Home
+export default Home;
