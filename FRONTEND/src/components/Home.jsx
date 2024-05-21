ecdec67f2e9881216e570813a9f3cfae179d6c3e
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-05-21 09:14:43 +0200

    Endra navn på navigasjon

--- FRONTEND/src/components/Home.jsx
+++ FRONTEND/src/components/Home.jsx
@@ -20,8 +20,7 @@ function Home() {
 
   return (
     <>
-      <h1>Home</h1>
-      <SearchResult pokemons={pokemons}/>
+    <SearchResult pokemons={pokemons}/>
       <section>
         <h2>Main Pokemons</h2>
         <section className="pokemon-container">
